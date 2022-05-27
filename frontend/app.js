114e9e510020b4766fb2eb14e45a7a21313aa9e0
IT20911744 <88874812+IT20911744@users.noreply.github.com>
2022-05-27 07:02:17 +0530

    App.js commit

--- frontend/app.js
+++ frontend/app.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./Components/Header";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import AboutUs from "./Components/AboutUs/AboutUs";
+import Hcontent from "./Components/HomeContent/Hcontent";
+import NavBar from "./Components/NavBar/NavBar";
+import Logins from "./Components/Home/Login";
+import Userlist from "./Components/DisplayUsers/DisplayUsers";
+import registerstudent from "./Components/Register/RegStudent";
+import registerSupervisor from "./Components/Register/RegSupervisor";
+import registerpanelMember from "./Components/Register/RegPanelmember";
 
 function App() {
   return (
     <Router>
       <switch>
         <Route path="/" exact component={Header} />
+        <Route path="/" exact component={NavBar} />
+        <Route path="/aboutUs" exact component={AboutUs} />
+        <Route path="/" exact component={Hcontent} />
+        <Route path="/" exact component={Logins} />
+        <Route path="/displayUlist" exact component={Userlist} />
+        <Route path="/regstudent" exact component={registerstudent} />
+        <Route path="/regsupervisor" exact component={registerSupervisor} />
+        <Route path="/regpanel" exact component={registerpanelMember} />
+        <Route path="/" component={Footer} />
       </switch>
     </Router>
   );
