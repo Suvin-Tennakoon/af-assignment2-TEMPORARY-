3e5e6f6985c106fa43bbf5afdd5b476cd91cd9ed
IT20911744 <88874812+IT20911744@users.noreply.github.com>
2022-06-01 19:38:22 +0530

    add

--- frontend/Components/SupViewMarkingShem/SupervisorMarkingShem.jsx
+++ frontend/Components/SupViewMarkingShem/SupervisorMarkingShem.jsx
@@ -1,80 +1,95 @@
-import React, {Component} from "react";
-import {Link, useParams, useHistory} from "react-router-dom";
+import React, { Component, useState } from "react";
+import { Link, useParams, useHistory } from "react-router-dom";
 
 import axios from "axios";
 
-const SupMarking = (props) => {
-
+const Marking = (props) => {
+  return props.marking.markingCriteria.map((mc) => {
     return (
-        props.marking.markingCriteria.map((mc) => {
-            return (
-                <tr>
-                    <td className="csan">{mc.markedArea}</td>
-                    <td className="csan"><input placeholder={`0/${mc.marksOutOf}`}/></td>
-                </tr>
-            )
-        })
-    )
-
+      <tr>
+        <td className="csan">{mc.markedArea}</td>
+        <td className="csan">
+          <input placeholder={`0/${mc.marksOutOf}`} />
+        </td>
+      </tr>
+    );
+  });
 };
 
-class SupMarkingSchema extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {SupMarkingshem: []};
-    }
+class MarkingSchema extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { Markingshem: [] };
+  }
 
-    componentDidMount() {
-        axios
-            .get("http://localhost:3001/api/markingScheme/getAll")
-            .then((res) => {
-                //get all info about a student
-                this.setState({SupMarkingshem: res.data});
+  componentDidMount() {
+    axios
+      .get("http://localhost:3001/api/markingScheme/getAll")
+      .then((res) => {
+        //get all info about a student
+        this.setState({ Markingshem: res.data });
+      })
+      .then((res) => {
+        console.log(this.state.Markingshem);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-            }).then((res) => {
-            console.log(this.state.SupMarkingshem);
-        })
-            .catch((err) => {
-                console.log(err);
-            });
+  Calculation() {}
+  SupervisorMarkingschem() {
+    return this.state.Markingshem.map((currentmarking) => {
+      if (currentmarking.submissionType == "")
+        return <Marking marking={currentmarking} />;
+    });
+  }
 
-    }
-
-    SupervisorMarkingschem() {
-        return this.state.SupMarkingshem.map((currentmarking) => {
-            if (currentmarking.submissionType == '')
-                return <SupMarking supmarking={currentmarking}/>;
-        });
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <br/>
-                <br/>
-                <table class="chamoditable table table-hover">
-                    <thead className="thead-dark">
-                    <tr className="table-dark">
-                        <th className="csan" scope="col">
-                            Marking Criteria
-                        </th>
-                        <th className="csan" scope="col">
-                            Marks
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-
-                    {this.SupervisorMarkingschem()}
-
-                    </tbody>
-                </table>
-                <button  className="btn btn-primary">Calculation</button>
-                <br/>
-                <br/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container">
+        <br />
+        <br />
+        <table class="chamoditable table table-hover">
+          <thead className="thead-dark">
+            <tr className="table-dark">
+              <th className="csan" scope="col">
+                Marking Criteria
+              </th>
+              <th className="csan" scope="col">
+                Marks
+              </th>
+            </tr>
+          </thead>
+          <tbody>{this.SupervisorMarkingschem()}</tbody>
+        </table>
+        <button type="button" className="btn btn-primary btn-sm">
+          Calculate
+        </button>
+        <br />
+        <br />
+        <lable>Group ID</lable>
+        <br />
+        <input></input>
+        <br />
+        <br />
+        <lable>Total Marks</lable>
+        <br />
+        <input></input>
+        <br />
+        <br />
+        <center>
+          <button type="button" className="btn btn-primary btn-sm">
+            Submit
+          </button>
+          <br />
+          <br />
+        </center>
+        <br />
+        <br />
+      </div>
+    );
+  }
 }
 
-export default SupMarkingSchema;
+export default MarkingSchema;
