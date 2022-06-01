a7c687bf3a81916059ccc56fa06f2dc65e02af75
IT20911744 <88874812+IT20911744@users.noreply.github.com>
2022-06-01 10:52:12 +0530

    add

--- frontend/Components/PanelViewMarkingSchem/ViewMarkingSchem.jsx
+++ frontend/Components/PanelViewMarkingSchem/ViewMarkingSchem.jsx
@@ -1,33 +1,25 @@
 import React, { Component } from "react";
 import { Link, useParams, useHistory } from "react-router-dom";
 
-
 import axios from "axios";
 
 const Marking = (props) => {
- 
   const data = [props];
 
-  return (
-   
-        
-      data.marking.map((mark)=>{
+  return data.marking.map((mark) => {
     <tr>
       <td className="csan">{mark.markingCriteria.markedArea}</td>
-      <td className="csan"><input  placeholder={mark.markingCriteria.marksOutOf}/></td>
-    </tr>
-      })
-   
-     
-    
-  );
+      <td className="csan">
+        <input placeholder={mark.markingCriteria.marksOutOf} />
+      </td>
+    </tr>;
+  });
 };
-
 
 class MarkingSchema extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { Markingshem: []};
+    this.state = { Markingshem: [] };
   }
 
   componentDidMount() {
@@ -36,14 +28,13 @@ class MarkingSchema extends React.Component {
       .then((res) => {
         //get all info about a student
         this.setState({ Markingshem: res.data });
-        
-      }).then((res)=>{
+      })
+      .then((res) => {
         console.log(this.state.Markingshem);
       })
       .catch((err) => {
         console.log(err);
       });
-    
   }
 
   PanelMarkingschem() {
@@ -68,11 +59,7 @@ class MarkingSchema extends React.Component {
               </th>
             </tr>
           </thead>
-          <tbody>
-          
-            {this.PanelMarkingschem()}
-            
-          </tbody>
+          <tbody>{this.PanelMarkingschem()}</tbody>
         </table>
         <br />
         <br />
